70d6bf09a447b2e51f56fd37c8d5cf9a65f16bb2
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-06-26 21:07:42 +0200

    Adapt it to PS2 code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,7 @@
-#include <pspkernel.h>
-#include <pspdisplay.h>
-#include <pspdebug.h>
-
 #include <string>
+#include <debug.h>
+#include <unistd.h>
 
-PSP_MODULE_INFO("PSP VARIABLE ISSUE", 0, 1, 0);
-PSP_MAIN_THREAD_ATTR(THREAD_ATTR_USER);
 
 // If these two lines are here, the code doesn't work fine
 // If you move these lines into main(), the code works fine
@@ -15,15 +11,16 @@ std::string str2 = "Hello2";
 int main(int argc, char *argv[])
 {   
     auto str3 = std::string("Hello3");
-    pspDebugScreenInit(); // Initialize the debug screen
-    pspDebugScreenPrintf("---\n");
-    pspDebugScreenPrintf(str.c_str()); // Print nothing or lot of spaces
-    pspDebugScreenPrintf("\n---\n");
-    pspDebugScreenPrintf(str2.c_str()); // Print nothing or lot of spaces
-    pspDebugScreenPrintf("\n---\n");
-    pspDebugScreenPrintf(str3.c_str()); // Print "Hello3"
+    init_scr(); // Initialize the debug screen
+	scr_printf("\n\n\n STATIC INIT example!\n\n\n");
+    scr_printf("---\n");
+    scr_printf(str.c_str()); // Print nothing or lot of spaces
+    scr_printf("\n---\n");
+    scr_printf(str2.c_str()); // Print nothing or lot of spaces
+    scr_printf("\n---\n");
+    scr_printf(str3.c_str()); // Print "Hello3"
     while (true)
     {
-        sceDisplayWaitVblankStart();
+        sleep(10);
     }
 }
